5ebbc78c4e910d5b9ce87082bb97df6ba355e18a
anil94sahu <anil94sahu@gmail.com>
2020-08-26 22:28:39 +0530

    api issue fixed

--- src/app/services/google-sheet.service.ts
+++ src/app/services/google-sheet.service.ts
@@ -8,7 +8,7 @@ export class GoogleSheetService {
 
   //https://docs.google.com/spreadsheets/d/e/2PACX-1vTTqgQBNNhYJDtNnkALP7x2OFr5-t-3raRsWe1nL-MG2QJQDUYA3MzhXEGbbZrpXVJg6neDQEWHzbtD/pubhtml
   // sheedID = '2PACX-1vTTqgQBNNhYJDtNnkALP7x2OFr5-t-3raRsWe1nL-MG2QJQDUYA3MzhXEGbbZrpXVJg6neDQEWHzbtD';
-  url = `https://sheet.best/api/sheets/8e7aae91-bfeb-417d-9503-83e81a8c123f`;
+  url = `https://sheetdb.io/api/v1/lzzl55j6l7va5`;
 
   constructor(private http: HttpClient) {
 
